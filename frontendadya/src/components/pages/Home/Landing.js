949a2023f62b561afbafffaa8549f92f44f485f6
Cibiyanna <pkcibiyanna@gmail.com>
2024-05-08 22:04:11 +0530

    tailwind screen attribute add for responsiveness

--- frontendadya/src/components/pages/Home/Landing.js
+++ frontendadya/src/components/pages/Home/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div className='mb-sm-5 h-[85vh] bg-hero-image bg-no-repeat bg-cover z-10' rel="preload">
+        <div className='mb-sm-5 h-[85vh] md:bg-hero-image bg-no-repeat bg-cover z-10' rel="preload">
             <div className='flex flex-col gap-[16px] mt-[40px]'>
                 <h2 className='logo text-center mb-md-4 text-[40px] font-bold text-green-500'>SKC Insurance</h2>
                 <h1 className='motto text-center text-[32px] font-mediums text-gray-00'>We Promise To Keep You Alright</h1>
