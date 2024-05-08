a08d649a36c3fcd1608ff6253a686937a066ba88
Cibiyanna <pkcibiyanna@gmail.com>
2024-05-08 21:03:23 +0530

    nav middle element bug fix

--- frontendadya/src/components/layouts/Header.js
+++ frontendadya/src/components/layouts/Header.js
@@ -1,30 +1,35 @@
-import { Link , useLocation } from "react-router-dom";
-import InstagramIcon from "../../icons/instagramIcon";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';    
+import { Link , useLocation , useNavigate } from "react-router-dom";   
 import {useSelector} from 'react-redux'
 import Avatar from '@mui/material/Avatar';
-
+import { deleteCookie } from "../../utils/cookie.service";
 const Header = () =>{
     const location = useLocation();
     console.log(location.pathname); 
+    const navigate = useNavigate()
     const {userData} = useSelector(store=>store.userData)
+
+
+    const handleLogout=()=>{
+        deleteCookie('token')
+        navigate('/signin')
+    }
+
     return(
         <>
             <header className="border-b-2 border-gray-300 shadow-lg">
-                <nav className="flex flex-row justify-between text-lg py-[16px] px-[20px]">
+                <nav className="flex flex-row justify-between text-lg py-[16px] px-[20px] relative ">
                     <div className="flex flex-row gap-[32px]">
                         <Link to="/" className="text-2xl font-semibold flex flex-row gap-[16px] items-center">
                             <img width="48" height="48" src="https://img.icons8.com/fluency/48/security-checked--v1.png" alt="security-checked--v1" />
                             <text>SKC Insurance</text>
                         </Link>
                     </div>
-                    <div className="flex flex-row gap-[24px] items-center text-[18px] mr-[140px]">
+                    <div className="flex flex-row gap-[24px] items-center text-[18px] absolute  left-[43%] top-[30%]">
                         <Link to="/" className={`${location.pathname === '/' ? 'text-green-600' : ''}`}>Home</Link>
                         <Link to={'/user-insurance'} className={`${location.pathname === '/user-insurance' ? 'text-green-600' : ''}`}>
                             <text className="">Applied</text>
                         </Link>
-                        {/* <Link to={'/contact'} className={`${location.pathname === '/contact' ? 'text-green-600' : ''}`}>Contact</Link> */}
-                        <Link to={'/signin'} className="hover:text-green-600">Login</Link>
+                        <button onClick={handleLogout} className="">Logout</button>
                         
                     </div>
                     <div className="flex items-center">
